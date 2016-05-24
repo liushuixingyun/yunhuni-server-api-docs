810cce0267731052725578fc19f97d865173181e
tanbro <RealTanbro@GMail.com>
2016-05-24 16:58:52 +0800

    disable githubpages extention

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
