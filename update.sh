60d6a3abf5241beb3fbecd812dd56c47d30a813e
Lluvio <caotousir@gmail.com>
2016-09-28 14:38:29 +0800

    update sh

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 #DEFAULT_BRANCH=master
 #BRANCH=${1:-$DEFAULT_BRANCH}
 #git checkout --force $BRANCH
-git pull origin $(git_current_branch)
+git_current_branch = git branch | grep \* | cut -d ' ' -f2
+git pull origin $git_current_branch
 #git reset --hard origin/$BRANCH
 npm install
 gitbook install
