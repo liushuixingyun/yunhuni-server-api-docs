e1d93b1e8454e40c7cc40e708344c2da9e95aa35
Lluvio <caotousir@gmail.com>
2016-09-29 14:57:03 +0800

    取消当前分支的所有操作，保持与远程分支同步

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
 # 默认分支
-DEFAULT_BRANCH=master
+#DEFAULT_BRANCH=master
 # 当前分支
 CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 # 目标分支，使用参数[1]，如没有参数，使用当前分支作为目标分支
-TARGET_BRANCH=${1:-$CURRENT_BRANCH}
+#TARGET_BRANCH=${1:-$CURRENT_BRANCH}
 # 检出服务器上最新的目标分支
 git fetch
-git checkout --force $BRANCH
-git reset --hard origin/$BRANCH
+git checkout --force $CURRENT_BRANCH
+git reset --hard origin/$CURRENT_BRANCH
 # npm 准备
 npm install
 # gitbook 构建
